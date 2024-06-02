b7a6387bad75be7b84f59a0e62a58c4b0e794e84
Shonsu <shonsu82@gmail.com>
2024-06-02 13:56:12 +0200

    add shared context for RestaurantControllerTests

--- RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
+++ RestaurantAPI.IntegrationTests/RestaurantControllerTests.cs
@@ -3,8 +3,16 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace RestaurantAPI.IntegrationTests;
 
-public class RestaurantControllerTests
+public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Startup>>
 {
+    private readonly HttpClient _client;
+
+    public RestaurantControllerTests(WebApplicationFactory<Startup> factory)
+    {
+        // var factory = new WebApplicationFactory<Startup>();
+        _client = factory.CreateClient();
+    }
+
     [Theory]
     [InlineData("pageNumber=1&pageSize=5")]
     [InlineData("pageNumber=2&pageSize=5")]
@@ -12,11 +20,9 @@ public class RestaurantControllerTests
     public async void GetAll_WithQyeryParameters_ReturnsOkResult(string queryParams)
     {
         // arrange
-        var factory = new WebApplicationFactory<Startup>();
-        HttpClient client = factory.CreateClient();
 
         // act
-        HttpResponseMessage response = await client.GetAsync("/api/restaurant?" + queryParams);
+        HttpResponseMessage response = await _client.GetAsync("/api/restaurant?" + queryParams);
 
         // assert
         response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
@@ -30,11 +36,9 @@ public class RestaurantControllerTests
     public async void GetAll_WithInvalidQueryParams_ReturnsBadRequest(string queryParams)
     {
         // arrange
-        var factory = new WebApplicationFactory<Startup>();
-        HttpClient client = factory.CreateClient();
 
         // act
-        HttpResponseMessage response = await client.GetAsync("/api/restaurant?" + queryParams);
+        HttpResponseMessage response = await _client.GetAsync("/api/restaurant?" + queryParams);
 
         // assert
         response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
